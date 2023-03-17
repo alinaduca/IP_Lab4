006d9b1fc872124005bc0ffccdedefb166ca6d3c
Iacob Eliza-Maria <100303019+melizai@users.noreply.github.com>
2023-03-17 11:12:03 +0200

    SecurityControll added

---
+++ SecurityControll/Person.java
@@ -0,0 +1,22 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 3/17/2023 11:10:04 AM
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Person.java
+
+import java.util.Vector;
+
+public class Person
+{
+
+    public Person()
+    {
+    }
+
+    public String getName()
+    {
+        return null;
+    }
+
+    public String name;
+    public Vector mySecurityControll;
+}
---
+++ SecurityControll/SecurityControll.java
@@ -0,0 +1,29 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 3/17/2023 11:11:01 AM
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   SecurityControll.java
+
+import java.util.Vector;
+
+public class SecurityControll
+{
+
+    public SecurityControll()
+    {
+    }
+
+    public void Login()
+    {
+    }
+
+    public void resetPassword(String s)
+    {
+    }
+
+    public boolean isLoggedIn(String s)
+    {
+        return false;
+    }
+
+    public Vector myPerson;
+}
